bc1bafd234e2e4c7de275455fc200ef5be52f65c
James Hugman <james@hugman.tv>
2014-10-13 23:13:03 +0100

    Initial commit.

---
+++ overidify-rules.js
@@ -0,0 +1,15 @@
+module.exports = {
+  platform: {
+    'ios': ['ios', 'webview', 'webkit'],
+    'android': ['android', 'webview', 'blink', 'webkit'],
+    'fxos': ['fxos', 'nativejs', 'gecko'],
+    'fennec': ['fxos', 'nativejs', 'gecko'],
+    'qml': ['qml', 'nativejs'],
+    'blackberry': ['blackberry', 'webview'],
+    'blackberry10': ['blackberry10', 'webview'],
+    'wp8': ['wp8', 'windows', 'ie', 'webview'],
+    'wp7': ['wp7', 'windows', 'ie', 'webview'],
+    'tizen': ['tizen', 'webview']
+  },  
+  flavor: ['dev', 'debug', 'test', 'stage', 'production' ]
+};
